a10113caf6ee1802e6d3c68d7bd77b884523b637
Vaibhav Saxena <saxena.vaibhav96@gmail.com>
2017-11-18 20:10:11 +0530

    changed generated data

--- generated_data/gen-nonlinear_sep.py
+++ generated_data/gen-nonlinear_sep.py
@@ -2,35 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas
 
-size1 = 1000
-size2 = 1000
+size1 = 2000
+size1_test = 500
+size2 = 2000
+size2_test = 500
 
 mean = [0.2, 0.2]
 cov = [[0.1, 0], [0, 0.1]]
 x1, y1 = np.random.multivariate_normal(mean, cov, size1/2).T
+x1_test, y1_test = np.random.multivariate_normal(mean, cov, size1_test/2).T
 mean = [1.4, 1.4]
 cov = [[0.1, 0], [0, 0.1]]
 x11, y11 = np.random.multivariate_normal(mean, cov, size1/2).T
+x11_test, y11_test = np.random.multivariate_normal(mean, cov, size1_test/2).T
 x1 = np.concatenate((x1,x11))
 y1 = np.concatenate((y1,y11))
+x1_test = np.concatenate((x1_test,x11_test))
+y1_test = np.concatenate((y1_test,y11_test))
 
 
 mean = [1.4, 0.2]
 cov = [[0.1, 0], [0, 0.1]]
 x2, y2 = np.random.multivariate_normal(mean, cov, size2/2).T
+x2_test, y2_test = np.random.multivariate_normal(mean, cov, size2_test/2).T
 mean = [0.2, 1.4]
 cov = [[0.1, 0], [0, 0.1]]
 x22, y22 = np.random.multivariate_normal(mean, cov, size2/2).T
+x22_test, y22_test = np.random.multivariate_normal(mean, cov, size2_test/2).T
 x2 = np.concatenate((x2,x22))
 y2 = np.concatenate((y2,y22))
+x1_test = np.concatenate((x1_test,x11_test))
+y1_test = np.concatenate((y1_test,y11_test))
 
 label0 = np.zeros(len(x1), dtype=np.int)
 label1 = np.ones(len(x2), dtype=np.int)
+label0_test = np.zeros(len(x1_test), dtype=np.int)
+label1_test = np.ones(len(x2_test), dtype=np.int)
 
 # plt.plot(x1, y1, 'ro')
 # plt.plot(x2, y2, 'go')
 # plt.show()
 
+# train data
 x1 = np.array(x1).reshape((x1.shape[0], 1)) # 1000x1
 y1 = np.array(y1).reshape((1, y1.shape[0])) # 1x1000
 label0 = label0.reshape((1, label0.shape[0]))
@@ -44,4 +57,22 @@ X2 = np.concatenate((x2,y2.T), axis=1) # 1000x2
 X2 = np.concatenate((X2,label1.T), axis=1) # 1000x3
 
 X = np.concatenate((X1, X2), axis=0)
-pandas.DataFrame(X).to_csv("well_separated_nonlinear.csv", header=None, index=False)
+np.random.shuffle(X)
+pandas.DataFrame(X).to_csv("well_separated_nonlinear.trn", header=None, index=False)
+
+# test data
+x1_test = np.array(x1_test).reshape((x1_test.shape[0], 1)) # 1000x1
+y1_test = np.array(y1_test).reshape((1, y1_test.shape[0])) # 1x1000
+label0_test = label0_test.reshape((1, label0_test.shape[0]))
+X1_test = np.concatenate((x1_test,y1_test.T), axis=1) # 1000x2
+X1_test = np.concatenate((X1_test,label0_test.T), axis=1) # 1000x3
+
+x2_test = np.array(x2_test).reshape((x2_test.shape[0], 1)) # 1000x1
+y2_test = np.array(y2_test).reshape((1, y2_test.shape[0])) # 1x1000
+label1_test = label1_test.reshape((1, label1_test.shape[0]))
+X2_test = np.concatenate((x2_test,y2_test.T), axis=1) # 1000x2
+X2_test = np.concatenate((X2_test,label1_test.T), axis=1) # 1000x3
+
+X = np.concatenate((X1_test, X2_test), axis=0)
+np.random.shuffle(X)
+pandas.DataFrame(X).to_csv("well_separated_nonlinear.tst", header=None, index=False)
