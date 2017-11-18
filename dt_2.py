8d9228a98db9f1e2c31fbcdf4e1277e329f31085
Aneesh Barthakur <abarthakur96@gmail.com>
2017-11-18 17:35:07 +0530

    Restructes dt_2.py and adds Prediction rule 4.
    This commit -
    1. Changes dt_2 to only retain code related to prediction rules
    2. Changes the functions to return accuracy

--- dt_2.py
+++ dt_2.py
@@ -1,50 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas
+
 import math
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import backend as K
 
 from sklearn.neighbors import NearestNeighbors
-
-
-
-
-class Ensemble :
-
-	def __init__(self,num_classes):
-		self.num_classifiers=0
-		self.num_classes=num_classes
-		self.classifier_list=[]
-
-	def add_classifier(self,classifier):
-		self.classifier_list.append(classifier)
-		self.num_classifiers=self.num_classifiers+1
-
-
-	def get_profile(self,sample):
-		#Decision profile
-		DP = np.zeros([self.num_classifiers, self.num_classes])
-		for i in range(0,self.num_classifiers):
-			#base classifier type
-			classifier=self.classifier_list[i]
-			#numpy array size 1 x num_classes
-			prediction=classifier.predict(sample)
-			DP[i,:]=prediction
-		return DP
-
-
-#not sure this is necessary yet, but oh well
-class KerasClassifier :
-
-	#wrap trained model in this class
-	def __init__(self,model,num_classes):
-		self.model=model
-		self.num_classes=num_classes
-
-	def predict(self,sample):
-		return self.model.predict(np.array([sample])).reshape((1,self.num_classes))
 
 
 #build decision templates
@@ -107,8 +69,7 @@ def train_and_test_0(trainX,trainY,testX,testY,voting_choice=0,k_val=5):
 	print("Prediction rule 0")
 	print("Total test accuracy is :")
 	print(accuracy)
-
-
+	return accuracy
 
 ####Prediction rule 1 : Decision Templates (Kuncheva 2001)
 def train_and_test_1(ens,num_classifiers,num_classes,trainX,trainY,testX,testY):
@@ -121,12 +82,9 @@ def train_and_test_1(ens,num_classifiers,num_classes,trainX,trainY,testX,testY):
 	for i in range(0,tot_test):
 		sample=testX[i]
 		DP= ens.get_profile(sample)
-		# print(DT)
-		# print(DP)
 		distances = np.sum(((DT - DP)**2),axis=(1,2))
 		label = np.argmin(distances,axis=(0))
 		true_label=np.argmax(testY[i])
-		# print(distances)
 		# print("Prediction :"+str(label))
 		# print("Actual :"+str(true_label))
 		if label==true_label:
@@ -136,18 +94,16 @@ def train_and_test_1(ens,num_classifiers,num_classes,trainX,trainY,testX,testY):
 	print("Prediction rule 1")
 	print("Total test accuracy is :")
 	print(accuracy)
-
+	return accuracy
 
 ####Prediction rule 2.1 : create DTs from the kNN in feature space
 def train_and_test_2_1(ens,num_classifiers,num_classes,trainX,trainY,testX,testY,k_val=5):
 	#build nearest neighbours data structure for efficient search
 	nbrs = NearestNeighbors(n_neighbors=k_val, algorithm='ball_tree').fit(trainX)
-
 	#predict
 	tot_test=testX.shape[0]
 	# tot_test=10
 	correct=0
-
 	for i in range(0,tot_test):
 		sample=testX[i]
 
@@ -158,17 +114,11 @@ def train_and_test_2_1(ens,num_classifiers,num_classes,trainX,trainY,testX,testY
 
 		#build decision templates
 		DT_nbrs=build_decision_templates(ens,num_classifiers,num_classes,X_nbrs,Y_nbrs)
-
 		DP= ens.get_profile(sample)
-		# print(DT)
-		# print(DP)
 		distances = np.sum(((DT_nbrs - DP)**2),axis=(1,2))
 
 		label = np.argmin(distances,axis=(0))
 		true_label=np.argmax(testY[i])
-
-		# print(distances)
-
 		# print("Prediction :"+str(label))
 		# print("Actual :"+str(true_label))
 		if label==true_label:
@@ -178,9 +128,7 @@ def train_and_test_2_1(ens,num_classifiers,num_classes,trainX,trainY,testX,testY
 	print("Prediction rule 2.1")
 	print("Total test accuracy is :")
 	print(accuracy)
-
-
-
+	return accuracy
 
 ####Prediction rule 3 : find k-NN in DP space. find which classifiers did well for the corresponding points,
 ####					and then weigh them accordingly ending in a weighted majority voting
@@ -192,20 +140,16 @@ def train_and_test_3(ens,num_classifiers,num_classes,trainX,trainY,testX,testY,k
 
 	#build nearest neighbours data structure for efficient search
 	nbrs = NearestNeighbors(n_neighbors=k_val, algorithm='ball_tree').fit(DP_X_flat)
-
 	#predict
 	tot_test=testX.shape[0]
 	# tot_test=10
 	correct=0
-
 	for i in range(0,tot_test):
 		sample=testX[i]
 		DP=ens.get_profile(sample)
 		DP_flat= np.reshape(DP,(1,num_classifiers*num_classes))
 
 		[dist_dp_nbrs,indices]= nbrs.kneighbors(DP_flat)
-		# print(indices)
-		# print(dist_dp_nbrs)
 
 		X_nbrs = trainX[indices[0]]
 		Y_nbrs= trainY[indices[0]]
@@ -249,84 +193,70 @@ def train_and_test_3(ens,num_classifiers,num_classes,trainX,trainY,testX,testY,k
 	print("Prediction rule 3")
 	print("Total test accuracy is :")
 	print(accuracy)
+	return accuracy
 
+####Prediction rule 4 : Given a sample, for each classifier, find the kNN in the projection space of the NN. See how well the classifier
+####					performs on these kNN, and weight the classifier accordingly
+def train_and_test_4(ens,num_classifiers,num_classes,trainX,trainY,testX,testY,k_val=5):
+	projections = []
+	nbrs = []
 
-def get_data(filename): # Satimage dataset
-    data = pandas.read_csv(filename, sep=r"\s+", header=None)
-    data = data.values
+	for keras_classifier in ens.classifier_list:
+		model = keras_classifier.model
+		get_proj = K.function([model.layers[0].input],
+	                                  [model.layers[-2].output])
+		proj = get_proj([trainX])[0]
+		projections.append(proj)
+		nbrs.append( NearestNeighbors(n_neighbors=k_val, algorithm='ball_tree').fit(proj) )
 
-    dataX = np.array(data[:,range(data.shape[1]-1)])
-    dataY = np.array(data[np.arange(data.shape[0]),data.shape[1]-1])
+	# #predict
+	tot_test=testX.shape[0]
+	# # tot_test = 30
+	correct=0
 
-    # convert dataY to one-hot, 6 classes
-    num_classes = 6
-    dataY = np.array([x-2 if x==7 else x-1 for x in dataY]) # re-named class 7 to 6(5)
-    dataY_onehot = np.zeros([dataY.shape[0], num_classes])
-    dataY_onehot[np.arange(dataY_onehot.shape[0]), dataY] = 1
+	for i in range(0,tot_test):
+		sample=testX[i]
+		sample=np.reshape(sample,(1,sample.shape[0]))
+		classifier_scores = np.zeros(num_classifiers)
+		#for each classifier, find k-NN in proj space
+		for c in range(num_classifiers):
+			model= ens.classifier_list[c].model
+			get_proj = K.function([model.layers[0].input],
+	                                  [model.layers[-2].output])
+			# print("shape")
+			proj = get_proj([sample])[0]
+			[dist_dp_nbrs,indices]= nbrs[c].kneighbors(proj)
 
-    return dataX, dataY_onehot
+			X_nbrs = trainX[indices[0]]
+			Y_nbrs= trainY[indices[0]]
 
+			#check which nbrs the classifier did well on
+			for j in range(0,X_nbrs.shape[0]): #to get prediction of classifier c for nbrs
+				xnbr= X_nbrs[j]
+				proj_xnbr_c=ens.get_profile(xnbr)[c] #num_classes size
+				prediction = np.argmax(proj_xnbr_c)
+				truth = np.argmax(Y_nbrs[j]) #because Y labels are one-hot
+				classifier_scores[c] += int(prediction==truth)
 
-#get training data
-trainX, trainY = get_data("data/sat.trn")
-testX, testY = get_data("data/sat.tst")
-num_classes = trainY.shape[1]
+		# print(sample.shape)
+		sample=testX[i]
+		DP=ens.get_profile(sample)
+		predictions = np.argmax(DP,axis=1) #by each classifier
+		predictions_one_hot= np.zeros([num_classifiers,num_classes])
+		for j in range(0,num_classifiers):
+			predictions_one_hot[j][predictions[j]]=1
+		
+		weighted_votes = np.multiply(predictions_one_hot,np.reshape(classifier_scores,(-1,1)))
+		total_votes=np.sum(weighted_votes,axis=0)
+		label = np.argmax(total_votes,axis=(0))
+		true_label=np.argmax(testY[i])
+		if label==true_label:
+			correct+=1
+		
+		# print(correct)
 
-#build ensemble
-def build_ensemble_1(trainX,trainY,num_classes):
-	###############Build ensemble of MLPs with bagging (sort of)#####################
-	ens = Ensemble(num_classes)
-	num_classifiers=10
-	half=int(num_classifiers/2)
-
-	#employ bagging
-	total_samples = trainX.shape[0]
-	max_samples = (total_samples/2)
-	max_samples=math.ceil(max_samples)
-
-	#NN with single hidden layer,trained on all features x 5
-	epochs=30
-	for i in range(0,half):
-		bag = np.random.randint(low=0, high=total_samples-1, size=max_samples)
-		tX_part= trainX[bag]
-		tY_part= trainY[bag]
-		model1 = Sequential()
-		model1.add(Dense(8, input_dim=trainX.shape[1], activation="sigmoid"))
-		model1.add(Dense(num_classes, activation="softmax"))
-		model1.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
-		model1.fit(tX_part,tY_part, epochs=epochs, batch_size=10) # epochs=150
-
-		ens.add_classifier(KerasClassifier(model1,num_classes))
-
-	#NN with 2 hidden layers x 5
-	for i in range(half,num_classifiers):
-		bag = np.random.randint(low=0, high=total_samples-1, size=max_samples)
-		tX_part= trainX[bag]
-		tY_part= trainY[bag]
-		model2 = Sequential()
-		model2.add(Dense(8, input_dim=trainX.shape[1], activation="sigmoid"))
-		model2.add(Dense(8, activation="sigmoid"))
-		model2.add(Dense(num_classes, activation="softmax"))
-		model2.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
-		model2.fit(tX_part, tY_part, epochs=epochs, batch_size=10) # epochs=150
-
-		ens.add_classifier(KerasClassifier(model2,num_classes))
-	#################################################################3
-	return ens
-
-
-ens = build_ensemble_1(trainX,trainY,num_classes)
-num_classifiers=ens.num_classifiers
-
-
-
-train_and_test_1(ens,num_classifiers,num_classes,trainX,trainY,testX,testY)
-train_and_test_2_1(ens,num_classifiers,num_classes,trainX,trainY,testX,testY,k_val=10)
-train_and_test_0(trainX,trainY,testX,testY,0,k_val=10)
-train_and_test_0(trainX,trainY,testX,testY,1,k_val=10)
-train_and_test_3(ens,num_classifiers,num_classes,trainX,trainY,testX,testY,k_val=10)
-
-
-
-##Prediction rule 2.1 : weighted voting for DPs (simple voting is simple kNN)
-
+	accuracy=float(correct)/tot_test
+	print("Prediction rule 5")
+	print("Total test accuracy is :")
+	print(accuracy)
+	return accuracy
